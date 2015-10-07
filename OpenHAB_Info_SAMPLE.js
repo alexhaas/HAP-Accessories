3d08a8040e25c05f48c364d93aea90c211bd83bf
michaelrcarroll <me@michaelrcarroll.com>
2015-10-07 01:18:03 -0700

    initial support for OpenHAB simple lights. will need to be enhanced, but basic support works.

---
+++ OpenHAB_Info_SAMPLE.js
@@ -0,0 +1,3 @@
+var exports = module.exports = {};
+
+exports.openHabIP = "127.0.0.1"; //The IP of your OpenHAB setup (localhost). If you need username and password use "username:password@127.0.0.1"
