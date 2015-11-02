9afea3951af9e4ee5bb62169a7b1a684ffa43d2b
Alex Haas <ahaas@apple.com>
2015-11-01 22:59:47 -0800

    Removing unused accessories.

--- OpenHAB_Info_SAMPLE.js
+++
@@ -1,3 +0,0 @@
-var exports = module.exports = {};
-
-exports.openHabIP = "127.0.0.1"; //The IP of your OpenHAB setup (localhost). If you need username and password use "username:password@127.0.0.1"
